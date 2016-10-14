4d9680de47cfcacca19565c6b088ee08af095e74
MisterAlex95 <alex.dana@epitech.eu>
2016-10-14 13:25:08 -0400

    * new: Add public tab to get coconuts

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,19 +5,22 @@ public class Spawner : MonoBehaviour {
 
 	public Sprite coconutSprite = null;
 	private GameObject coconut = null;
+    public GameObject[] coconuts = new GameObject[9];
 
 	// Use this for initialization
 	void Start () {
 		coconut = new GameObject();
 		coconut.AddComponent<SpriteRenderer>().sprite = coconutSprite;
+        coconut.name = "Coconut";
 
 		for (int i = 0; i < 9; i++) {
 			Vector3 position = new Vector3(Random.Range(0.0f, 1600.0f), Random.Range(0.0f, -1600.0f), 0);
 			GameObject newCoconut = (GameObject)Instantiate(coconut, position, new Quaternion());
 			newCoconut.name = "Coconut[" + i + "]";
 			newCoconut.transform.localScale = new Vector3(33, 33, 0);
-		}
-	}
+            coconuts[i] = newCoconut;
+        }
+    }
 
 	// Update is called once per frame
 	void update () {
