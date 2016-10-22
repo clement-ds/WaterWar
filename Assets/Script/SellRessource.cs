17b34f717323c4889756215952a31f0d8741b3ce
Carapuce <garance.vaugier@epitech.eu>
2016-10-22 02:19:03 +0200

    Basic Inventory
    Clic sur le marché pour donner des noix de coco, clic sur le bateau pour toggle l'inventaire.

---
+++ Assets/Script/SellRessource.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SellRessource : MonoBehaviour {
+    public Canvas inventory;
+    public GameObject slot;
+    public GameObject item;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+            {
+                GameObject panel = GameObject.Find("SlotsPanel");
+                GameObject a = (GameObject)Instantiate(slot);
+                a.transform.SetParent(panel.transform, false);
+                GameObject b = (GameObject)Instantiate(item);
+                b.transform.SetParent(a.transform, false);
+            }
+        }
+    }
+}
---
+++ Assets/Script/ToggleInventoryOnClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleInventoryOnClick : MonoBehaviour {
+    public Canvas inventory;
+    // Use this for initialization
+    void Start () {
+        inventory.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos)) { 
+                inventory.enabled = (inventory.enabled == false);
+            }
+        }
+    }
+}
