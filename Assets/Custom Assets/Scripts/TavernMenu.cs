b08d269c10084ec98b6c1c29f8dd5e29f77630a7
DESKTOP-57RNAH8\Joseph <joseph.barbisan@epitech.eu>
2016-11-20 16:55:45 +0900

    *new : Basic crew menu

---
+++ Assets/Custom Assets/Scripts/TavernMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+namespace UnityStandardAssets.Utility
+{
+
+    public class TavernMenu : MonoBehaviour
+    {
+
+        void Start()
+        {
+            inventory.enabled = false;
+        }
+
+        public Canvas inventory;
+
+        void OnMouseDown()
+        {
+            inventory.enabled = (inventory.enabled == false);
+        }
+    }
+}
