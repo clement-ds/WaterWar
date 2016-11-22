01e26402b8378d9e6d219e1a901b88f67fbde374
DESKTOP-57RNAH8\Joseph <joseph.barbisan@epitech.eu>
2016-11-22 12:54:21 +0900

    basic ship movement for one island

---
+++ Assets/Custom Assets/Scripts/Island1.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Island1 : MonoBehaviour {
+
+    public GameObject dockingTrigger;
+    public GameObject playerShip;
+
+    private Rigidbody2D playerRb2d;
+
+	// Use this for initialization
+	void Start () {
+        playerRb2d = playerShip.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        //print("dockingtrigger : " + dockingTrigger.transform.localPosition);
+        //Vector2 dock;
+        //dock.x = dockingTrigger.transform.localPosition.x;
+        //dock.y = dockingTrigger.transform.localPosition.y;
+        //print("dock : " + dock);
+        //playerRb2d.MovePosition(dock);
+        playerShip.transform.position = Vector3.MoveTowards(playerShip.transform.position, dockingTrigger.transform.position, 10000);
+    }
+
+}
