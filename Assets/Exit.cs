e70d31f77dcd84cc4213bd71ea10c4769fde5527
DESKTOP-57RNAH8\Joseph <joseph.barbisan@epitech.eu>
2016-11-22 20:24:50 +0900

    *new : Added exit trigger to interaction scene.

---
+++ Assets/Exit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+    public GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseDown()
+    {
+        gm.GoWorldMap();
+    }
+}
