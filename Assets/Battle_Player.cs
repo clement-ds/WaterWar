a8b11f7731fe6d00283831fe67f4ab1a22eb4859
MisterAlex95 <alex.dana@epitech.eu>
2016-12-15 13:49:02 -0500

    * new : Add movement, life and action button to battle_scene

--- Assets/Battle_Player.cs
+++ Assets/Battle_Player.cs
@@ -1,23 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Battle_Player : MonoBehaviour {
+    public Slider slider = null;
+    public Text distanceText = null;
+    public Button actionFuite = null;
+    public Button actionAbordage = null;
 
-	public int life = 100;
-    public int position = 2;
+    private int life = 100;
+    private int position = 2;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-    public int getCurrentLife()
-    {
+    // Use this for initialization
+    void Start () {
+        slider.value = life;
+        distanceText.text = position.ToString();
+        actionFuite.gameObject.SetActive(false);
+        actionAbordage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        slider.value = life;
+        distanceText.text = position.ToString();
+        if (position == 3) {
+            actionFuite.gameObject.SetActive(true); 
+            actionAbordage.gameObject.SetActive(false);
+        } else if (position == 1) {
+            actionFuite.gameObject.SetActive(false);
+            actionAbordage.gameObject.SetActive(true);
+        } else {
+            actionFuite.gameObject.SetActive(false);
+            actionAbordage.gameObject.SetActive(false);
+        }
+
+    }
+
+    public int getCurrentLife() {
         return life;
     }
+
+    public void approcheAction() {
+        if (position < 3) position++;
+    }
+
+    public void eloignementAction() {
+        if (position > 1) position--;
+    }
+
 }
---
+++ Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public Slider slider = null;
+
+    private int life = 100;
+
+    // Use this for initialization
+    void Start () {
+        slider.value = life;
+     }
+
+    // Update is called once per frame
+    void Update () {
+        slider.value = life;
+    }
+}
