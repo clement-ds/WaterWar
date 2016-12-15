7d27acad1b19a75df2fa4c40065900a7660ebb94
MisterAlex95 <alex.dana@epitech.eu>
2016-12-14 23:49:14 -0500

    Add battle scene

---
+++ Assets/Battle_Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battle_Player : MonoBehaviour {
+
+	public int life = 100;
+    public int position = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int getCurrentLife()
+    {
+        return life;
+    }
+}
