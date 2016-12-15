7cf24145ed6b8dc004251ac3e0656ebf96644f9c
Carapuce <garance.vaugier@epitech.eu>
2016-12-15 21:54:55 +0100

    Canons boum boum

---
+++ Assets/Script/FiringCanons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class FiringCanons : MonoBehaviour {
+    public GameObject MainCanon { get; set; }
+
+    void Start()
+    {
+        MainCanon = null;
+    }
+
+    // Use this for initialization
+    public void noCanon()
+    {
+        MainCanon = null;
+    }
+
+    public void fireOn(GameObject target)
+    {
+        print("Hellloo");
+        if (MainCanon != null)
+        {
+            print("Canon " + MainCanon.name + " fires on " + target.name);
+        }
+          
+        else
+            print("No Canon");
+    }
+}
+
---
+++ Assets/Script/Target.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+    FiringCanons player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("PlayerController").GetComponent<FiringCanons>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+            {
+                player.fireOn(this.gameObject);
+            }
+        }
+    }
+}
