6165c5aa2692c7b34666f9a06bde82518b6c8e82
MisterAlex95 <alex.dana@epitech.eu>
2017-01-14 15:10:50 -0500

    * new: add Cooldown script

---
+++ Assets/Script/Battle/Cooldown.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cooldown : MonoBehaviour {
+
+    public float timeLeft = 30.0f;
+    private float startTime = 0;
+    private bool possibility = false;
+
+    // Use this for initialization
+    void Start() {
+        startTime = timeLeft;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0) {
+            possibility = true;
+        } else {
+            possibility = false;
+        }
+    }
+
+    bool getPossibility() {
+        if (possibility == true)
+        {
+            timeLeft = startTime;
+            possibility = false;
+        }
+        return possibility;    
+    }
+}
