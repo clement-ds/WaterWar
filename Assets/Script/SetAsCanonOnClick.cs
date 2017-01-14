0681c281d4846eed2321ff69a49bf286515221e5
Carapuce <garance.vaugier@epitech.eu>
2017-01-14 20:10:10 +0100

    Sprite outline on select
    :D

--- Assets/Script/SetAsCanonOnClick.cs
+++ Assets/Script/SetAsCanonOnClick.cs
@@ -19,7 +19,12 @@ public class SetAsCanonOnClick : MonoBehaviour {
             Vector2 touchPos = new Vector2(wp.x, wp.y);
             if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
             {
+                if (player.MainCanon)
+                {
+                    player.MainCanon.GetComponent<SpriteOutline>().enabled = false;
+                }
                 player.MainCanon = this.gameObject;
+                player.MainCanon.GetComponent<SpriteOutline>().enabled = true;
             }
         }
     }
