af2cbd6c4054d844c7033149507bcbd4f1f53a69
Carapuce <garance.vaugier@epitech.eu>
2017-01-15 00:17:08 +0100

    Tooltip position

--- Assets/Script/Tooltip.cs
+++ Assets/Script/Tooltip.cs
@@ -5,24 +5,66 @@ using UnityEngine.UI;
 public class Tooltip : MonoBehaviour {
     // Use this for initialization
     public SetAsCanonOnClick canon;
+    public GameObject next = null;
+    private bool clicked = false;
+    private GameObject visibility;
 
     void Start()
     {
-        this.gameObject.SetActive(false);
+        visibility = this.transform.GetChild(0).gameObject;
+        visibility.SetActive(false);
     }
 
-    public void show (GameObject obj) {
-        this.gameObject.SetActive(true);
-
-        float xPos = obj.transform.position.x + 42;
-        float yPos = obj.transform.position.y - obj.GetComponent<RectTransform>().sizeDelta.y - 42;
+    private void show (GameObject obj) {
+        visibility.SetActive(true);
 
         canon = obj.GetComponent<SetAsCanonOnClick>();
-        //this.transform.position = new Vector2(xPos, yPos);
+
+        Debug.Log(obj.transform.position);
+
+         float xPos = obj.transform.position.x / 4.5f * (679 / 2) + 10;
+         float yPos = obj.transform.position.y / 9.36450662739f * (1413 / 2) + 10; //SA MERE LA PUUUUUUUTTTE
+
+         this.transform.localPosition = new Vector3(xPos + 42, yPos - 42, 0);
     }
 	
-	// Update is called once per frame
-	public void hide () {
-        this.gameObject.SetActive(false);
-	}
+    public void showOn(GameObject obj)
+    {
+        next = obj;
+    }
+
+    // Update is called once per frame
+    public void hideOn(GameObject obj)
+    {
+        if (next == obj)
+        {
+            next = null;
+        }
+    }
+
+    public void hide()
+    {
+        visibility.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (clicked == true)
+        {
+            if (next != null)
+            {
+                show(next);
+            }
+            else
+            {
+                hide();
+            }
+            next = null;
+            clicked = false;
+        }
+        if (Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(0))
+        {
+            clicked = true;
+        }
+    }
 }
--- Assets/Script/Tooltippable.cs
+++ Assets/Script/Tooltippable.cs
@@ -4,17 +4,29 @@ using System.Collections;
 public class Tooltippable : MonoBehaviour {
     Tooltip tooltip;
     // Use this for initialization
+    public string tooltipName = "Tooltip";
     void Start () {
-        tooltip = GameObject.Find("Tooltip").GetComponent<Tooltip>();
+        tooltip = GameObject.Find(tooltipName).GetComponent<Tooltip>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            tooltip.hideOn(this.gameObject);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
             Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 touchPos = new Vector2(wp.x, wp.y);
             if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
             {
-                tooltip.show(this.gameObject);
+                tooltip.showOn(this.gameObject);
             }
+            else
+            {
+                tooltip.hideOn(this.gameObject);
+            }
+        }
     }
 }
