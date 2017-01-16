9af2880bba9753961356a925e8a4c670c13bb241
MisterAlex95 <alex.dana@epitech.eu>
2017-01-15 20:52:30 -0500

    * new: beginning of bullet display

--- Assets/Script/FiringCanons.cs
+++ Assets/Script/FiringCanons.cs
@@ -3,10 +3,16 @@ using System.Collections;
 using UnityEngine.EventSystems;
 
 public class FiringCanons : MonoBehaviour {
+    ArrayList bullets;
+    public Sprite bullet;
     public GameObject MainCanon { get; set; }
 
     void Start() {
         MainCanon = null;
+        bullets = new ArrayList();
+    }
+
+    void Update() {
     }
 
     // Use this for initialization
@@ -17,6 +23,10 @@ public class FiringCanons : MonoBehaviour {
     public void fireOn(GameObject target) {
         if (MainCanon != null && MainCanon.GetComponent<Cooldown>().getPossibility() == true) {
             Battle_Enemy enemy = target.GetComponentInParent<Battle_Enemy>();
+
+            bullet = new Sprite();
+            bullets.Add(bullet);
+
             print("Canon " + MainCanon.name + " fires on " + target.name + " with boulet " + MainCanon.GetComponent<SetAsCanonOnClick>().bouletname);
             if (enemy != null) {
                 enemy.setCurrentLife(enemy.getCurrentLife() - 20);
