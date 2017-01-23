245123e914e2f6744e67a1242a10d0bcf298ca86
Calvez kevin <kevin.calvez@epitech.eu>
2017-01-23 17:44:44 -0500

    * new add window before swirch scene

--- Assets/Script/FiringCanons.cs
+++ Assets/Script/FiringCanons.cs
@@ -7,6 +7,8 @@ public class FiringCanons : MonoBehaviour {
     public Sprite bullet;
     public GameObject MainCanon { get; set; }
     public GameManager gm;
+    public Rect windowRect = new Rect(Screen.width/2, Screen.height/2, 200, 60);
+    public bool GUIEnabled = false;
 
     void Start() {
         MainCanon = null;
@@ -22,22 +24,43 @@ public class FiringCanons : MonoBehaviour {
     }
 
     public void fireOn(GameObject target) {
-        if (MainCanon != null && MainCanon.GetComponent<Cooldown>().getPossibility() == true) {
+        if (MainCanon != null && MainCanon.GetComponent<Cooldown>().getPossibility() == true)
+        {
             Battle_Enemy enemy = target.GetComponentInParent<Battle_Enemy>();
 
             bullet = new Sprite();
             bullets.Add(bullet);
 
             print("Canon " + MainCanon.name + " fires on " + target.name + " with boulet " + MainCanon.GetComponent<SetAsCanonOnClick>().bouletname);
-            if (enemy != null) {
+            if (enemy != null)
+            {
                 enemy.setCurrentLife(enemy.getCurrentLife() - 20);
                 print("Aouch we loose 20 pv");
                 if (enemy.getCurrentLife() <= 0)
-                    gm.GoInteraction();
+                    GUIEnabled = true;
+
 
             }
-        } else
-            print("No Canon");
+        }
+        else {
+            OnGUI(); //print("No Canon");
+        }
     }
+    void OnGUI()
+    {
+        if (GUIEnabled)
+        windowRect = GUI.Window(0, windowRect, DoMyWindow, "Victory");
+    }
+
+    void DoMyWindow(int windowID)
+    {
+        if (GUI.Button(new Rect(10, 20, 100, 20), "Continue"))
+        {
+            print("Have a good day");
+            gm.GoInteraction();
+        }
+
+    }
+
 }
 
