3ad7afd8675e31e64dda35a3ab34d70a192687cf
Carapuce <garance.vaugier@epitech.eu>
2017-01-24 19:08:02 +0100

    Main Menu stylé

---
+++ Assets/ToggleChildren.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleChildren : MonoBehaviour {
+    public void ToggleAllChildren()
+    {
+        bool activate = this.gameObject.activeSelf == false;
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(activate);
+        }
+        this.gameObject.SetActive(activate);
+    }
+}
