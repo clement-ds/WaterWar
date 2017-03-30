e3750af9e9003150cc8900ba8ee66344bf54375b
MisterAlex95 <alex.dana@epitech.eu>
2017-03-29 23:41:30 -0400

    * new: add shop UI

---
+++ Assets/DisplayInventory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public int itemCount = 10, columnCount = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
---
+++ Assets/Script/ShopInteraction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopInteraction : MonoBehaviour {
+    GameObject shopMenu;
+    Inventory inventory;
+    public int ressourceNumber;
+
+    void Start () {
+        shopMenu = GameObject.Find("ShopCanvas");
+        shopMenu.SetActive(false);
+        inventory = GameObject.Find("Inventory System").GetComponent<Inventory>();
+    }
+
+    void FillSellShop() {
+        /*
+         * 
+         *
+         */
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos)) {
+                shopMenu.SetActive(!shopMenu.activeSelf);
+                 
+                //                inventory.AddItem(ressourceNumber);
+            }
+        }
+    }
+}
