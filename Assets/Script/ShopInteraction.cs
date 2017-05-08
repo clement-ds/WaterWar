0d8b11e76a8f12d20e1b948e05c7743992b361a7
DESKTOP-57RNAH8\Joseph <joseph.barbisan@epitech.eu>
2017-05-08 19:11:06 +0900

    island manger tied to ui alpha

--- Assets/Script/ShopInteraction.cs
+++ Assets/Script/ShopInteraction.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ShopInteraction : MonoBehaviour {
     GameObject shopMenu;
     Inventory inventory;
     public int ressourceNumber;
+    public GameObject shopList;
 
     void Start () {
         shopMenu = GameObject.Find("ShopCanvas");
@@ -13,10 +15,24 @@ public class ShopInteraction : MonoBehaviour {
     }
 
     void FillSellShop() {
-        /*
-         * 
-         *
-         */
+        IslandManager im = IslandManager.GetInstance();
+        IslandInventory inv = im.island.inventory;
+        GameObject item = GameObject.Find("item");
+
+        for (int i = 0; i < inv.food.Count; ++i)
+        {
+            //if (i == 0)
+            //{
+                InventoryObject invObj = inv.food[i];
+                Text text = item.GetComponent<Text>();
+                text.text = "";
+                text.text = invObj.name + " " + invObj.number;
+            //} else
+            //{
+
+            //}
+
+        }
     }
 
     void Update () {
@@ -26,7 +42,7 @@ public class ShopInteraction : MonoBehaviour {
             Vector2 touchPos = new Vector2(wp.x, wp.y);
             if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos)) {
                 shopMenu.SetActive(!shopMenu.activeSelf);
-                 
+                FillSellShop();
                 //                inventory.AddItem(ressourceNumber);
             }
         }
