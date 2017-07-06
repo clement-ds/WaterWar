cf4cb9268b6044616984c0d077c905420d2336b0
Clément <clement.da-silva@epitech.eu>
2017-07-06 12:17:52 +0200

    Added shot animation

---
+++ Assets/Script/Animations/ParticleLauncher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleLauncher : MonoBehaviour
+{
+
+    public ParticleEmitter[] emitters;
+
+    void Start()
+    {
+        AnimationEvent ae = new AnimationEvent(); ae.messageOptions = SendMessageOptions.DontRequireReceiver;
+    }
+
+    public void fireEverything()
+    {
+        foreach (ParticleEmitter emitter in emitters)
+        {
+            emitter.Emit();
+        }
+    }
+}
