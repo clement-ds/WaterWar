2e75b208858d226bb586a8d041b608746dcfa554
Joseph Barbisan <joseph.barbisan@outlook.com>
2017-07-13 14:18:12 +0200

    altfight

---
+++ Assets/Script/AltFight/unit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unit : MonoBehaviour {
+
+    public GameObject Selected;
+    public GameObject Unselected;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Vector3 clickedPosition = Input.mousePosition;
+        print("CLICK : " + clickedPosition);
+        Selected.SetActive(Unselected.activeSelf);
+        Unselected.SetActive(!Selected.activeSelf);
+    }
+
+}
