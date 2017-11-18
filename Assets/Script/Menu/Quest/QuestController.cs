20081c709b49ba6f2f31ce41cdadda5ddfa05572
Alex Dana <alex.dana@epitech.eu>
2017-11-18 22:30:08 +0100

    * new: add quest in book

--- Assets/Script/Menu/Quest/QuestController.cs
+++ Assets/Script/Menu/Quest/QuestController.cs
@@ -1,44 +1,114 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class QuestController : MonoBehaviour {
-  private int currentIsland;
-
-  public GameObject QuestItemPrefab;
-  public GameObject ContentPanel;
-
-  private QuestLog questLog;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
 
 
-  // Use this for initialization
-  void Start()
-  {
-    IslandManager manager = IslandManager.GetInstance();
-    currentIsland = PlayerManager.GetInstance().player.currentIsland;
-    questLog = manager.islands[currentIsland].questLog;
-    FillQuestPanel();
-  }
+// works with ListPanelRoot/ListReceiverPanel/ListRowPanel
+public class QuestController : UIController
+{
+    private List<PlayerQuest> questList;
+    public Button closeButton;
+    public int nbrQuestByPage = 10;
+    private int currentPage = 0;
 
-  void FillQuestPanel()
-  {
-    Debug.Log(questLog.quests[0].description);
-    for (int i = 0; i < questLog.quests.Count; ++i)
-    {
-      GameObject newItem = Instantiate(QuestItemPrefab) as GameObject;
-      QuestItemController controller = newItem.GetComponent<QuestItemController>();
-
-      controller.name = questLog.quests[i].ToString();
-      controller.title.text = questLog.quests[i].ToString();
-      controller.objective.text = questLog.quests[i].description;
-//      controller.reward.text = questLog.quests[i].reward.name;
-
-      newItem.transform.SetParent(ContentPanel.transform);
-      newItem.transform.localScale = Vector3.one;
+    void Start() {
+        FillItems();
     }
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
-  }
+    public override void Populate()
+    {
+        base.Populate();
+        FillItems();
+    }
+
+
+    private void FillItems()
+    {
+        Debug.Log("----------------Nbr of quests: " + PlayerManager.GetInstance().player.questLog.quests.Count);
+        questList = PlayerManager.GetInstance().player.questLog.quests;
+
+        int i = 0;
+
+        foreach (PlayerQuest quest in questList)
+        {
+            i++;
+            if (i >= (nbrQuestByPage * currentPage) && i < (nbrQuestByPage * currentPage + nbrQuestByPage))
+            {
+                GameObject questRow = (GameObject)Instantiate(rowPrefab);
+
+                foreach (Transform child in questRow.transform)
+                {
+                    if (child.name == "MemberObjectif")
+                    {
+                        Text text = (Text)child.GetComponent<Text>();
+                        text.text = quest.objective;
+                    }
+                    else if (child.name == "MemberDescription")
+                    {
+                        Text text = (Text)child.GetComponent<Text>();
+                        text.text = quest.description;
+
+                    }
+                    else if (child.name == "MemberItemReward")
+                    {
+                        Image img = (Image)child.GetComponent<Image>();
+                        img.sprite = Resources.Load<Sprite>("Sprites/quest");
+                    }
+                    else if (child.name == "MemberDescription")
+                    {
+                        Text text = (Text)child.GetComponent<Text>();
+                        text.text = quest.description;
+                    }
+                    else if (child.name == "MemberMoneyReward")
+                    {
+                        Text text = (Text)child.GetComponent<Text>();
+                        text.text = "+" + quest.moneyReward + "£";
+                    }
+                    else if (child.name == "AcceptButton")
+                    {
+                        Button AcceptButton = (Button)child.GetComponent<Button>();
+                        CreateClosureForAccept(quest, AcceptButton);
+                    }
+                }
+                questRow.transform.SetParent(panel.transform, false);
+                questRow.SetActive(true);
+            }
+        }
+    }
+
+    // Necessary because of unity bug in lambda
+    void CreateClosureForAccept(PlayerQuest quest, Button button)
+    {
+        button.onClick.AddListener(() =>
+        {
+            Player player = PlayerManager.GetInstance().player;
+
+            quest.taken = true;
+            player.questLog.quests.Add(quest);
+            IslandManager.GetInstance().islands[player.currentIsland].questLog.quests.Remove(quest);
+            Populate();
+        });
+    }
+    // -----------------------------------------
+
+    public void OnClick()
+    {
+        TogglePanel();
+        closeButton.gameObject.SetActive(panel.gameObject.active);
+    }
+
+    public void NextPage()
+    {
+        ++currentPage;
+        Populate();
+    }
+
+    public void PrevPage()
+    {
+        --currentPage;
+        Populate();
+    }
+
 }
