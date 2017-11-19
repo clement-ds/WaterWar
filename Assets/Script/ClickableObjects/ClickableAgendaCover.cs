b53475dac83b891f84a9effa0576a03ab4f8e163
DESKTOP-BQODNS3\dsccd <clement.da-silva@epitech.eu>
2017-11-19 17:45:13 +0100

    Quest book animation/Camera works

---
+++ Assets/Script/ClickableObjects/ClickableAgendaCover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableAgendaCover : MonoBehaviour {
+    private Animator anim;
+    private bool opened = false;
+    private CameraAnimationManager cameraAnim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cameraAnim = GameObject.Find("MainCamera").GetComponent<CameraAnimationManager>();
+    }
+
+    void OnMouseOver()
+    {
+        //TODO: Glow
+        if (Input.GetMouseButtonDown(0)) {
+            opened = !opened;
+            anim.Play(opened ? "OpenAgenda" : "CloseAgenda");
+            cameraAnim.PlayAnimation(opened ? "CameraToQuest" : "CameraFromQuestToDefault", PlayType.CROSSFADE);
+        }
+    }
+}
