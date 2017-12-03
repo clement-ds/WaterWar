b0c567d5cb971b2e98eeeceeab8503c7f8aa378c
DESKTOP-BQODNS3\dsccd <clement.da-silva@epitech.eu>
2017-12-03 14:20:43 +0100

    Add wealthGenerator, Remove unused library

--- Assets/Script/IntroSceneManager.cs
+++ Assets/Script/IntroSceneManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 
 public class IntroSceneManager : MonoBehaviour {
-    public GameObject map, menu;
+    public GameObject map, menu, CoinPrefab;
     private CameraAnimationManager cameraManager;
+    public GameObject wealthSpawnPoint;
+
+    private int wealth;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +19,35 @@ public class IntroSceneManager : MonoBehaviour {
         {
             cameraManager.PlayAnimation("CameraStart");
         }
+
+        InvokeRepeating("UpdateWealth", 0f, 1f);
+        InvokeRepeating("SpawnMoney", .5f, .2f);
+    }
+
+    private int moneyCurrentlySpawned = 0;
+    private const float positionOffset = .5f;
+    private List<GameObject> spawnedMoney;
+    private void SpawnMoney() {
+        if (moneyCurrentlySpawned < wealth) {
+            GameObject obj = GameObject.Instantiate(CoinPrefab);
+            obj.transform.SetParent(wealthSpawnPoint.transform, false);
+            obj.transform.eulerAngles = new Vector3(Random.Range(0, 180), Random.Range(0, 180), Random.Range(0, 180));
+            obj.transform.position += new Vector3(Random.Range(-positionOffset, positionOffset), Random.Range(-positionOffset, positionOffset), Random.Range(-positionOffset, positionOffset));
+            moneyCurrentlySpawned += 10;
+        }
+        if (moneyCurrentlySpawned > wealth + 10) {
+            GameObject maillonFaible = spawnedMoney[spawnedMoney.Count - 1];
+            spawnedMoney.Remove(maillonFaible);
+            GameObject.Destroy(maillonFaible);
+            moneyCurrentlySpawned -= 10;
+        }
+    }
+    private void UpdateWealth() {
+        wealth = PlayerManager.GetInstance().player.money;
     }
 
     void Update() {
-        if (GameManager.Instance.IsInGame() == 45) {
+        if (GameManager.Instance.IsInGame() == 45) { // TODO: remplacer ca par mouvement de la camera render / display/not display
             if (!map.activeSelf)
             {
                 map.SetActive(true);
@@ -30,4 +58,5 @@ public class IntroSceneManager : MonoBehaviour {
             }
         }
     }
+
 }
