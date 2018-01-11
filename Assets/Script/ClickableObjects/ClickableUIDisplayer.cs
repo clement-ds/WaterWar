becce0e837347bc21bf7c278d5f7e615104a7959
DESKTOP-BQODNS3\dsccd <clement.da-silva@epitech.eu>
2018-01-11 16:11:28 +0100

    Add shop & tavern to 3D scene

--- Assets/Script/ClickableObjects/ClickableUIDisplayer.cs
+++ Assets/Script/ClickableObjects/ClickableUIDisplayer.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ClickableUIDisplayer : MonoBehaviour {
 
 	public UIController controller; 
+	[SerializeField] private GameObject button;
 	// Use this for initialization
 
 	void OnMouseDown() {
-		controller.TogglePanel();
+		if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
+			if (button) {
+				button.SetActive(!button.activeSelf);
+			}
+			controller.TogglePanel();
+        }
 	}
 }
