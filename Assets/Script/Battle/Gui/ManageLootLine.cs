a4aaf3923b1bbc1107ecbca32f8dfa6178cafc79
andres_k <kevin.andres@epitech.eu>
2018-01-16 20:44:06 +0000

    * new: loot gui

---
+++ Assets/Script/Battle/Gui/ManageLootLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageLootLine : MonoBehaviour {
+    public Text lootName;
+    public Text lootNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void initLootLine(string lootName, int lootNumber)
+    {
+        this.lootName.text = lootName;
+        this.lootNumber.text = lootNumber.ToString();
+    }
+}
