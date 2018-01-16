fdf51fd0cdcbc124d7116d2a3ad8193bd70ae231
Alex Dana <alex.dana@epitech.eu>
2018-01-16 22:46:06 +0100

    * fuckit soundmanager

---
+++ Assets/Script/Sound/SoundManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class SoundManager : MonoBehaviour 
+{
+    private AudioSource audioSource;
+    public List<AudioClip> clips = null;
+
+    public void PlaySingle(string soundName)
+    {
+      AudioClip clip = clips.Find((c) => c.name == soundName);
+      if (clip) {
+        audioSource.clip = clip;
+        audioSource.Play();
+      }
+    }
+}
