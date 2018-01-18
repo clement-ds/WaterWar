1834473ce90d2f605426d7b552137cb9360b4462
Clement <clement.da-silva@epitech.eu>
2018-01-18 14:52:54 +0100

    Add start menu manager and fadeOut

---
+++ Assets/Script/Animations/PanelFader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelFader : MonoBehaviour {
+
+	[SerializeField] private CanvasGroup canvas;
+	[SerializeField] private float time = 2;
+
+	public void FadeOut() {
+		if (!canvas.gameObject.activeSelf) {
+			canvas.gameObject.SetActive(true);
+		}
+		canvas.alpha = 1;
+		StartCoroutine(FadeCoroutine(false));
+	}
+
+	public void FadeIn() {
+		if (!canvas.gameObject.activeSelf) {
+			canvas.gameObject.SetActive(true);
+		}
+		canvas.alpha = 0;
+		StartCoroutine(FadeCoroutine(true));
+	}
+
+	IEnumerator FadeCoroutine(bool fadeIn) {
+		while (fadeIn ? canvas.alpha < 1 : canvas.alpha > 0) {
+			Debug.Log(canvas.alpha);
+			float amount = Time.deltaTime / time;
+			canvas.alpha += fadeIn ? amount : -amount;
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Script/StartMenu/StartMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuManager : MonoBehaviour {
+
+	PanelFader fader;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("kikou");
+		fader = GetComponent<PanelFader>();
+		fader.FadeOut();
+	}
+	
+}
