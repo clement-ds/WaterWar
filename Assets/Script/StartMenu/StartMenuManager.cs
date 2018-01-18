2e36912ce325ca8c5f692a2bb921d33b1d87af11
Clement <clement.da-silva@epitech.eu>
2018-01-18 18:38:28 +0100

    Add working settings for music, fx and graphics

--- Assets/Script/StartMenu/StartMenuManager.cs
+++ Assets/Script/StartMenu/StartMenuManager.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartMenuManager : MonoBehaviour {
 
+public class StartMenuManager : MonoBehaviour {
+	public List<GameObject> canvases;
 	PanelFader fader;
 	// Use this for initialization
 	void Start () {
 		fader = GetComponent<PanelFader>();
 		fader.FadeOut();
+	}
+
+	public void CanvasSelector(int index) {
+		for (int i = 0; i < canvases.Count; i++) {
+			if (index == i) {
+				canvases[i].SetActive(!canvases[i].activeSelf);
+			} else {
+				canvases[i].SetActive(false);
+			}
+		}
+
 	}
 	
 }
