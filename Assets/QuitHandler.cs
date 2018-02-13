cb941c20c7b67473c916bcddc11e2c6642654fce
Clement <clement.da-silva@epitech.eu>
2018-02-13 21:01:07 +0100

    Loading after battle

---
+++ Assets/QuitHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitHandler : MonoBehaviour {
+
+	public GameObject loadingPanel;
+
+	public void QuitBattle() {
+		loadingPanel.SetActive(true);
+		GameManager.Instance.GoIntroMenu();
+	}
+}
